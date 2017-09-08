bc541c3d7b266c70df9720b17123e3d56e69d8fe
DESKTOP-T2QD4LG\daryong <daryong0905@gmail.com>
2017-09-08 22:41:51 +0900

    課題３完了

---
+++ 06Kim_ColLesson03/ColLesson03.java
@@ -0,0 +1,59 @@
+package ColLesson03;
+
+import java.util.Scanner;
+
+public class ColLesson03 {
+	public static void main(String[] args) {
+		// Wordクラスのインスタンス配列
+		Word[] words = new Word[5];
+
+		// コマンドラインから入力
+		System.out.println("英単語と日本語をスペースで区切って入力して下さい。");
+		Scanner sc  = new Scanner(System.in);
+		//String input = sc.nextLine();
+
+		// ここから記述してください
+		// 例えば、「apple  りんご」と入力されたときはtmp[0]に"apple"、tmp[1]に"りんご"が入る
+		//String[] tmp = input.split(" ");
+
+
+		//System.out.println(tmp[0]);
+		//System.out.println(tmp[1]);
+
+
+		int index = 0;
+		try{
+		while(true){
+			String input = sc.nextLine();
+			if(input.equals("e")){
+				break;
+			}
+			String[] tmp = input.split(" ");
+			words[index] = new Word();
+			words[index].setEnglish(tmp[0]);
+			words[index].setJapanese(tmp[1]);
+			index++;
+			System.out.println("次の単語を入力してください。\"e\"で終了します。");
+		}
+
+		for(int i = 0; i < index; i++){
+			System.out.println(
+					words[i].toString()
+					);
+		}
+
+		System.out.println(
+				index
+				+"件登録しました。");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			System.out.println("登録制限を超えました。登録済みのデータは以下になります。");
+			for(int j = 0; j < index; j++){
+				System.out.println(
+						words[j].toString()
+						);
+			}
+		}
+	}
+}
